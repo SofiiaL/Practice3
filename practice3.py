086e1a6b99d5052fde1f998a052c98442e3dfdb7
SofiiaL <104678651+SofiiaL@users.noreply.github.com>
2023-04-15 11:53:56 +0200

    Create practice3.py

---
+++ practice3.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+# Виконати запит на API та отримати відповідь
+response = requests.get('https://api.exchangeratesapi.io/latest')
+
+# Декодувати відповідь у форматі JSON
+data = json.loads(response.text)
+
+# Обробити та проаналізувати дані
+rates = data['rates']
+eur_rate = rates['EUR']
+usd_rate = rates['USD']
+
+# Зберегти дані у форматі JSON на локальному пристрої
+with open('rates.json', 'w') as f:
+    json.dump(rates, f)
+
+print(f'1 EUR = {eur_rate:.2f} USD')
+print(f'1 USD = {1/usd_rate:.2f} EUR')
